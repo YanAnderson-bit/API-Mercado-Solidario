55d46c48b5a9a76f473797d01d5d8119ba1815fd
YanAnderson-bit <yan.anderson135@gmail.com>
2024-05-27 21:44:05 -0300

    Applyes p-04 in formasDePagamento

--- src/main/java/com/mercado_solidario/api/repository/FormasDePagamentoRepository.java
+++ src/main/java/com/mercado_solidario/api/repository/FormasDePagamentoRepository.java
@@ -1,15 +1,12 @@
 package com.mercado_solidario.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.mercado_solidario.api.entity.FormasDePagamento;
 
 @Repository
-public interface FormasDePagamentoRepository extends JpaRepository<FormasDePagamento, Long>{
-	
-	List<FormasDePagamento> findAllByDescricaoContains(String descricao);
-
+public interface FormasDePagamentoRepository
+		extends JpaRepository<FormasDePagamento, Long>, JpaSpecificationExecutor<FormasDePagamento> {
 }
