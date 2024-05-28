9880e5ea6710fba8090b069aa50a4d8a52d01300
YanAnderson-bit <yan.anderson135@gmail.com>
2024-05-27 21:31:26 -0300

    Applys P-04 rule in cidades resource

--- src/main/java/com/mercado_solidario/api/repository/CidadeRepository.java
+++ src/main/java/com/mercado_solidario/api/repository/CidadeRepository.java
@@ -1,19 +1,11 @@
 package com.mercado_solidario.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.mercado_solidario.api.entity.Cidade;
 
 @Repository
-public interface CidadeRepository extends JpaRepository<Cidade, Long> {
-
-	List<Cidade> findAllByNomeContains(String nome);
-	
-	List<Cidade> findAllByEstadoNomeContains(String nome);
-	
-	//List<Cidade> findAllByEstadoId(Long id);//Não funciona
-	
+public interface CidadeRepository extends JpaRepository<Cidade, Long>, JpaSpecificationExecutor<Cidade> {
 }
