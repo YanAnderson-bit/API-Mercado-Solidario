43ea0df3b0f0ae935fbdce591b821d11eb239042
YanAnderson-bit <yan.anderson135@gmail.com>
2024-05-28 21:15:17 -0300

    Apply rule P-04 in fornecedores

--- src/main/java/com/mercado_solidario/api/controller/FornecedorControler.java
+++ src/main/java/com/mercado_solidario/api/controller/FornecedorControler.java
@@ -1,13 +1,19 @@
 package com.mercado_solidario.api.controller;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ReflectionUtils;
@@ -19,131 +25,143 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercado_solidario.api.entity.Cidade;
+import com.mercado_solidario.api.entity.Endereço;
+import com.mercado_solidario.api.entity.Estado;
 import com.mercado_solidario.api.entity.Fornecedor;
 import com.mercado_solidario.api.execption.EntidadeNaoEncontradaExeption;
 import com.mercado_solidario.api.repository.FornecedorRepository;
 import com.mercado_solidario.api.service.FornecedorServices;
 
-@RestController 
+@RestController
 @RequestMapping(value = "/fornecedores")
 public class FornecedorControler {
 
-	@Autowired 
+	@Autowired
 	private FornecedorRepository fornecedorRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private FornecedorServices fornecedorServices;
-	
+
 	@GetMapping
-	public List<Fornecedor> listar() {
-		return fornecedorRepository.findAll();
+	public List<Fornecedor> listar(
+			@RequestParam(required = false) String nome,
+			@RequestParam(required = false) String cidade,
+			@RequestParam(required = false) String estado) {
+		Specification<Fornecedor> spec = (root, query, criteriaBuilder) -> {
+			List<Predicate> predicates = new ArrayList<>();
+			if (nome != null) {
+				predicates.add(
+						criteriaBuilder.like(root.get("nome"), "%" + nome + "%"));
+			}
+			if (cidade != null) {
+				Join<Fornecedor, Endereço> enderecoJoin = root.join("endereço");
+				Join<Endereço, Cidade> cidadeJoin = enderecoJoin.join("cidade");
+				predicates.add(criteriaBuilder.like(cidadeJoin.get("nome"), "%" + cidade + "%"));
+			}
+			if (estado != null) {
+				Join<Fornecedor, Endereço> enderecoJoin = root.join("endereço");
+				Join<Endereço, Cidade> cidadeJoin = enderecoJoin.join("cidade");
+				Join<Cidade, Estado> estadoJoin = cidadeJoin.join("estado");
+				predicates.add(criteriaBuilder.like(estadoJoin.get("nome"), "%" + estado + "%"));
+			}
+			return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+		};
+		return fornecedorRepository.findAll(spec);
 	}
-	
-	@GetMapping("/{fornecedorId}") // -> /fornecedores/fornecedoreId 
+
+	@GetMapping("/{fornecedorId}") // -> /fornecedores/fornecedoreId
 	public ResponseEntity<Fornecedor> buscar(@PathVariable("fornecedorId") Long Id) {
 		Optional<Fornecedor> fornecedor = fornecedorRepository.findById(Id);
-		
-		if(fornecedor.isPresent()) {
+
+		if (fornecedor.isPresent()) {
 			return ResponseEntity.ok(fornecedor.get());
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
-	//Busca por componenetes
-	//  -> /fornecedores/por-nome?nome = nome_buscado
-	@GetMapping("/por-nome")
-	public List<Fornecedor> FornecedoesPorNome(String nome) { 
-		return fornecedorRepository.findAllByNomeContains(nome);
-	}
-	@GetMapping("/por-cidade")
-	public List<Fornecedor> FornecedoesPorCidade(String cidade) { 
-		return fornecedorRepository.findAllByEndereçoCidadeNomeContains(cidade);
-	}
-	@GetMapping("/por-estado")
-	public List<Fornecedor> FornecedoesPorEstado(String estado) { 
-		return fornecedorRepository.findAllByEndereçoCidadeEstadoNomeContains(estado);
-	}
-	
-	//Comando POST
-	@PostMapping 
+
+	// Comando POST
+	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Fornecedor adicionar(@RequestBody Fornecedor fornecedor) { 
-		/*Modelo:
-		{
-		    "nome":"nome",
-		    "endereço": {
-		    	"id" = id
-		    }
-		}
-		*/
+	public Fornecedor adicionar(@RequestBody Fornecedor fornecedor) {
+		/*
+		 * Modelo:
+		 * {
+		 * "nome":"nome",
+		 * "endereço": {
+		 * "id" = id
+		 * }
+		 * }
+		 */
 		return fornecedorServices.salvar(fornecedor);
 	}
-		
-	//Comandos PUT
-	@PutMapping("/{fornecedorId}") 
+
+	// Comandos PUT
+	@PutMapping("/{fornecedorId}")
 	public ResponseEntity<?> atualizar(@PathVariable("fornecedorId") Long Id, @RequestBody Fornecedor fornecedor) {
 		try {
 			Optional<Fornecedor> fornecedorAtual = fornecedorRepository.findById(Id);
-	
-			if(fornecedorAtual.isPresent()) {
+
+			if (fornecedorAtual.isPresent()) {
 				BeanUtils.copyProperties(fornecedor, fornecedorAtual.get(), "id", "endereço");
 				Fornecedor fornecedorSalvo = fornecedorServices.salvar(fornecedorAtual.get());
-				
+
 				return ResponseEntity.ok(fornecedorSalvo);
 			}
-			
+
 			return ResponseEntity.notFound().build();
 		} catch (EntidadeNaoEncontradaExeption e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
-		} 
-		
+		}
+
 	}
-	
-	@PatchMapping("/{fornecedorId}") 
-	public ResponseEntity<?> atualizaParcial(@PathVariable("fornecedorId") Long Id, @RequestBody Map<String, Object> campos) {
+
+	@PatchMapping("/{fornecedorId}")
+	public ResponseEntity<?> atualizaParcial(@PathVariable("fornecedorId") Long Id,
+			@RequestBody Map<String, Object> campos) {
 		Optional<Fornecedor> fornecedor = fornecedorRepository.findById(Id);
-	
-		if(fornecedor.isEmpty()) {
+
+		if (fornecedor.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
-		
-		merge(campos, fornecedor.get());		
-	
-		return atualizar(Id,fornecedor.get());
+
+		merge(campos, fornecedor.get());
+
+		return atualizar(Id, fornecedor.get());
 	}
-		
-		
+
 	private void merge(Map<String, Object> camposOrigem, Fornecedor fornecedorDestino) {
 		ObjectMapper objectMapper = new ObjectMapper();
 		Fornecedor fornecedorOrigem = objectMapper.convertValue(camposOrigem, Fornecedor.class);
-		
+
 		camposOrigem.forEach((nomePropiedade, valorPropiedade) -> {
 			Field field = ReflectionUtils.findField(Fornecedor.class, nomePropiedade);
-			field.setAccessible(true); 
-			Object novoValor = ReflectionUtils.getField(field,fornecedorOrigem);
-			
+			field.setAccessible(true);
+			Object novoValor = ReflectionUtils.getField(field, fornecedorOrigem);
+
 			ReflectionUtils.setField(field, fornecedorDestino, novoValor);
 		});
 	}
-	
-	//Comandos DELET
-	@DeleteMapping("/{fornecedorId}") 
+
+	// Comandos DELET
+	@DeleteMapping("/{fornecedorId}")
 	public ResponseEntity<Fornecedor> remover(@PathVariable("fornecedorId") Long Id) {
 		try {
-			
-			fornecedorServices.excluir(Id); 
+
+			fornecedorServices.excluir(Id);
 			return ResponseEntity.noContent().build();
 		} catch (EntidadeNaoEncontradaExeption e) {
 			return ResponseEntity.notFound().build();
 		} catch (DataIntegrityViolationException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
-		
+
 	}
-	
+
 }
--- src/main/java/com/mercado_solidario/api/repository/FornecedorRepository.java
+++ src/main/java/com/mercado_solidario/api/repository/FornecedorRepository.java
@@ -3,17 +3,12 @@ package com.mercado_solidario.api.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.mercado_solidario.api.entity.Fornecedor;
 
 @Repository
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long>, JpaSpecificationExecutor<Fornecedor> {
 
-	List<Fornecedor> findAllByNomeContains(String nome);
-	
-	List<Fornecedor> findAllByEndereçoCidadeNomeContains(String cidade);
-	
-	List<Fornecedor> findAllByEndereçoCidadeEstadoNomeContains(String estado);
-	
 }
